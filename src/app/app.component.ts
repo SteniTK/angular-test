6b25bb00be95ee6af17a0dd07f712c6dbf656598
Steni Koilraj <stenikoil96@gmail.com>
2020-09-27 13:56:26 +0400

    retrieve data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'angular-test';
+  menuItems = [];
+  constructor(private http: HttpClient) { 
+    this.http = http;
+  }
+
+  ngOnInit(): void {
+    this.getMenu();
+  }
+
+  getMenu(): void {
+    this.http.get<any>('http://208.109.13.111:9090/api/Category', {observe: 'body', responseType: 'json'}).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
